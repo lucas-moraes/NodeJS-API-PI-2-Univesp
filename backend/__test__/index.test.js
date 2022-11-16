dd70ea29e77944e75bd93c696ad52685693bc536
Lucas Moraes <lucas.moraes88@yahoo.com>
2022-11-15 22:19:40 -0300

    Implementação de testes para rota users

--- backend/__test__/index.test.js
+++ backend/__test__/index.test.js
@@ -31,7 +31,7 @@ describe("streetHole router tests", () => {
     return await request(server.app)
       .delete("/api/streetHole/deleteOne")
       .send({
-        endereco: "JEST",
+        id: "JEST",
       })
       .expect(200);
   });
@@ -40,7 +40,7 @@ describe("streetHole router tests", () => {
     return await request(server.app)
       .delete("/api/streetHole/deleteMany")
       .send({
-        endereco: "JEST",
+        id: "JEST",
       })
       .expect(200);
   });
